7ac37deada07d1830404362bcdd1c5a6cae6ece6
Ved7680 <ved.n@ahduni.edu.in>
2024-08-07 18:58:23 +0530

    User Entity + Table using Hibernate

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/newuser")
+	public String newUser() {
+		return "NewUser";
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users") // To make table
+public class UserEntity {
+	@Id // To set userId Primary Key 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // For auto increment of primary key
+	private Integer userId;
+	private String firstName;
+	private String email;
+	private String password;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
