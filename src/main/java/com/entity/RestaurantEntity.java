8ec77254bb549adcca76f321fbb4174558412479
Ved7680 <ved.n@ahduni.edu.in>
2024-08-30 18:06:03 +0530

    Length Validation - Restaurant Entity

--- src/main/java/com/entity/RestaurantEntity.java
+++ src/main/java/com/entity/RestaurantEntity.java
@@ -1,11 +1,14 @@
 package com.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name="restaurant")
@@ -16,12 +19,14 @@ public class RestaurantEntity {
 	private Integer restaurantId;
 	
 	@NotBlank(message = "Please Enter Restaurant Name")
+	@Pattern(regexp="[a-zA-Z\\s]+", message="Please Enter Valid Restaurant Name")
 	private String name;
 	
 	@NotBlank(message = "Please Enter Restaurant Address")
 	private String address;
 	
 	@NotBlank(message = "Please Enter Restaurant Category")
+	@Length(min=3, max=20, message="Please Enter Category between 3 to 20 characters")
 	private String category;
 	
 	public Integer getRestaurantId() {
